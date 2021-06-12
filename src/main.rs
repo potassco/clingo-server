079fd3bc1287fe4d43dcd33474f2f2876ff554e9
Sven Thiele <sthiele78@gmail.com>
2021-06-12 13:35:16 +0200

    port to rocket 0.5

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use]
 extern crate rocket;
 #[macro_use]
@@ -12,10 +10,11 @@ use convert::{
     json_to_assignment, json_to_assumptions, json_to_configuration_result, json_to_parts,
     json_to_symbol,
 };
+use parking_lot::Mutex;
+use rocket::data::ToByteUnit;
+use rocket::serde::json::Json;
 use rocket::{Data, State};
-use rocket_contrib::json::Json;
-use std::io::Read;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use utils::{ConfigurationResult, ModelResult, RequestId, ServerError, Solver, StatisticsResult};
 
 #[cfg(test)]
@@ -25,84 +24,69 @@ mod test;
 fn index(id: &RequestId) -> String {
     format!("This is request #{}.", id.0)
 }
-
 #[get("/create")]
-fn create(state: State<Arc<Mutex<Solver>>>) -> Result<String, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
+fn create(state: &State<Arc<Mutex<Solver>>>) -> Result<String, ServerError> {
+    let mut solver = state.lock();
     solver.create(vec!["0".to_string()])?;
     Ok("Created clingo Solver.".to_string())
 }
 #[post("/add", data = "<data>")]
-fn add(state: State<Arc<Mutex<Solver>>>, data: Data) -> Result<String, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
-    let mut buf = String::new();
-    let mut ds = data.open();
-    ds.read_to_string(&mut buf)?;
-    solver.add("base", &[], &buf)?;
+async fn add(state: &State<Arc<Mutex<Solver>>>, data: Data<'_>) -> Result<String, ServerError> {
+    let ds = data.open(512.kibibytes());
+    let cap = ds.into_string().await?;
+
+    state.lock().add("base", &[], &cap.into_inner())?;
     Ok("Added data to Solver.".to_string())
 }
 #[post("/ground", format = "application/json", data = "<data>")]
-fn ground(state: State<Arc<Mutex<Solver>>>, data: Data) -> Result<String, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
-
-    let mut buf = String::new();
-    let mut ds = data.open();
-    ds.read_to_string(&mut buf)?;
-    let val = serde_json::from_str(&buf).map_err(|_| ServerError::InternalError {
+async fn ground(state: &State<Arc<Mutex<Solver>>>, data: Data<'_>) -> Result<String, ServerError> {
+    let ds = data.open(512.kibibytes());
+    let cap = ds.into_string().await?;
+    let val = serde_json::from_str(&cap.into_inner()).map_err(|_| ServerError::InternalError {
         msg: "Could not parse json data",
     })?;
 
     let parts = json_to_parts(&val)?;
     // ground the parts
+    let mut solver = state.lock();
     solver.ground(&parts)?;
     Ok("Grounding.".to_string())
 }
-
 #[post("/assign_external", format = "application/json", data = "<data>")]
-fn assign_external(state: State<Arc<Mutex<Solver>>>, data: Data) -> Result<String, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
-
-    let mut buf = String::new();
-    let mut ds = data.open();
-    ds.read_to_string(&mut buf)?;
-    let val = serde_json::from_str(&buf).map_err(|_| ServerError::InternalError {
+async fn assign_external(
+    state: &State<Arc<Mutex<Solver>>>,
+    data: Data<'_>,
+) -> Result<String, ServerError> {
+    let ds = data.open(512.kibibytes());
+    let cap = ds.into_string().await?;
+    let val = serde_json::from_str(&cap.into_inner()).map_err(|_| ServerError::InternalError {
         msg: "Could not parse json data",
     })?;
 
     let assignment = json_to_assignment(&val)?;
+    let mut solver = state.lock();
     solver.assign_external(&assignment)?;
     Ok("External assigned.".to_string())
 }
 #[post("/release_external", format = "application/json", data = "<data>")]
-fn release_external(state: State<Arc<Mutex<Solver>>>, data: Data) -> Result<String, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
-
-    let mut buf = String::new();
-    let mut ds = data.open();
-    ds.read_to_string(&mut buf)?;
-    let val = serde_json::from_str(&buf).map_err(|_| ServerError::InternalError {
+async fn release_external(
+    state: &State<Arc<Mutex<Solver>>>,
+    data: Data<'_>,
+) -> Result<String, ServerError> {
+    let ds = data.open(512.kibibytes());
+    let cap = ds.into_string().await?;
+    let val = serde_json::from_str(&cap.into_inner()).map_err(|_| ServerError::InternalError {
         msg: "Could not parse json data",
     })?;
 
     let symbol = json_to_symbol(&val)?;
+    let mut solver = state.lock();
     solver.release_external(&symbol)?;
     Ok("External released.".to_string())
 }
 #[get("/solve")]
-fn solve(state: State<Arc<Mutex<Solver>>>) -> Result<String, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
+fn solve(state: &State<Arc<Mutex<Solver>>>) -> Result<String, ServerError> {
+    let mut solver = state.lock();
     solver.solve(SolveMode::ASYNC | SolveMode::YIELD, &[])?;
     Ok("Solving.".to_string())
 }
@@ -111,63 +95,50 @@ fn solve(state: State<Arc<Mutex<Solver>>>) -> Result<String, ServerError> {
     format = "application/json",
     data = "<data>"
 )]
-fn solve_with_assumptions(
-    state: State<Arc<Mutex<Solver>>>,
-    data: Data,
+async fn solve_with_assumptions(
+    state: &State<Arc<Mutex<Solver>>>,
+    data: Data<'_>,
 ) -> Result<String, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
-    let mut buf = String::new();
-    let mut ds = data.open();
-    ds.read_to_string(&mut buf)?;
-    let val = serde_json::from_str(&buf).map_err(|_| ServerError::InternalError {
+    let ds = data.open(512.kibibytes());
+    let cap = ds.into_string().await?;
+    let val = serde_json::from_str(&cap.into_inner()).map_err(|_| ServerError::InternalError {
         msg: "Could not parse json data",
     })?;
 
     let assumptions = json_to_assumptions(&val)?;
+    let mut solver = state.lock();
     solver.solve_with_assumptions(&assumptions)?;
     Ok("Solving with assumptions.".to_string())
 }
 #[get("/model")]
-fn model(state: State<Arc<Mutex<Solver>>>) -> Result<Json<ModelResult>, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
+fn model(state: &State<Arc<Mutex<Solver>>>) -> Result<Json<ModelResult>, ServerError> {
+    let mut solver = state.lock();
     match solver.model() {
         Ok(mr) => Ok(Json(mr)),
         Err(e) => Err(e),
     }
 }
 #[get("/resume")]
-fn resume(state: State<Arc<Mutex<Solver>>>) -> Result<String, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
+fn resume(state: &State<Arc<Mutex<Solver>>>) -> Result<String, ServerError> {
+    let mut solver = state.lock();
     solver.resume()?;
     Ok("Search is resumed.".to_string())
 }
 #[get("/close")]
-fn close(state: State<Arc<Mutex<Solver>>>) -> Result<String, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
+fn close(state: &State<Arc<Mutex<Solver>>>) -> Result<String, ServerError> {
+    let mut solver = state.lock();
     solver.close()?;
     Ok("Solve handle closed.".to_string())
 }
 #[get("/register_dl_theory")]
-fn register_dl_theory(state: State<Arc<Mutex<Solver>>>) -> Result<String, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
+fn register_dl_theory(state: &State<Arc<Mutex<Solver>>>) -> Result<String, ServerError> {
+    let mut solver = state.lock();
     solver.register_dl_theory()?;
     Ok("Difference logic theory registered.".to_string())
 }
 #[get("/statistics")]
-fn statistics(state: State<Arc<Mutex<Solver>>>) -> Result<Json<StatisticsResult>, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
+fn statistics(state: &State<Arc<Mutex<Solver>>>) -> Result<Json<StatisticsResult>, ServerError> {
+    let mut solver = state.lock();
     match solver.statistics() {
         Ok(stats) => Ok(Json(stats)),
         Err(e) => Err(e),
@@ -175,36 +146,34 @@ fn statistics(state: State<Arc<Mutex<Solver>>>) -> Result<Json<StatisticsResult>
 }
 #[get("/configuration")]
 fn configuration(
-    state: State<Arc<Mutex<Solver>>>,
+    state: &State<Arc<Mutex<Solver>>>,
 ) -> Result<Json<ConfigurationResult>, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
+    let mut solver = state.lock();
     match solver.configuration() {
         Ok(stats) => Ok(Json(stats)),
         Err(e) => Err(e),
     }
 }
 #[post("/set_configuration", format = "application/json", data = "<data>")]
-fn set_configuration(state: State<Arc<Mutex<Solver>>>, data: Data) -> Result<String, ServerError> {
-    let mut solver = state
-        .lock()
-        .map_err(|_| ServerError::InternalError { msg: "PoisonError" })?;
-    let mut buf = String::new();
-    let mut ds = data.open();
-    ds.read_to_string(&mut buf)?;
-    let val = serde_json::from_str(&buf).map_err(|_| ServerError::InternalError {
+async fn set_configuration(
+    state: &State<Arc<Mutex<Solver>>>,
+    data: Data<'_>,
+) -> Result<String, ServerError> {
+    let ds = data.open(512.kibibytes());
+    let cap = ds.into_string().await?;
+    let val = serde_json::from_str(&cap.into_inner()).map_err(|_| ServerError::InternalError {
         msg: "Could not parse json data",
     })?;
 
     let c = json_to_configuration_result(&val)?;
+    let mut solver = state.lock();
     solver.set_configuration(&c)?;
     Ok("Set configuration.".to_string())
 }
-
-fn rocket() -> rocket::Rocket {
+#[launch]
+fn rocket() -> _ {
     let state: Arc<Mutex<Solver>> = Arc::new(Mutex::new(Solver::None));
-    rocket::ignite().manage(state).mount(
+    rocket::build().manage(state).mount(
         "/",
         routes![
             index,
@@ -224,8 +193,4 @@ fn rocket() -> rocket::Rocket {
             register_dl_theory
         ],
     )
-}
-
-fn main() {
-    rocket().launch();
 }
