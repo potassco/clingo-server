1223c4f7809fa39ce1fc6fdde36f2d67a82e1aa7
Sven Thiele <sthiele78@gmail.com>
2022-06-15 21:35:41 +0200

    Format with black

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ import traceback
 
 import requests
 
-server = 'http://localhost:8000/'
+server = "http://localhost:8000/"
 conf_fail = '{\
   "tester": {\
     "share": ""\
@@ -55,72 +55,91 @@ conf = '{\
 
 def main():
     try:
-        parser = argparse.ArgumentParser(
-            description="Test the clingo server")
-        parser.add_argument('-i', '--input', required=True,
-                            help='logic program')
+        parser = argparse.ArgumentParser(description="Test the clingo server")
+        parser.add_argument("-i", "--input", required=True, help="logic program")
         parser.add_argument(
-            '-c', '--conf',  action='store_true', required=False,
-            help='set and get configuration')
+            "-c",
+            "--conf",
+            action="store_true",
+            required=False,
+            help="set and get configuration",
+        )
         parser.add_argument(
-            '-s', '--stats',  action='store_true', required=False,
-            help='get statistics')
-
-        parser.add_argument('--assume',  action='store_true', required=False,
-                            help='assume queen(3,1) to be true')
-
-        parser.add_argument('--pigeons',  action='store_true', required=False,
-                            help='add parmeters for the pigeon example holes=3 pigeons=2')
-        parser.add_argument('--external',  action='store_true', required=False,
-                            help='assign external atom `enable` with True in the external example')
+            "-s", "--stats", action="store_true", required=False, help="get statistics"
+        )
 
         parser.add_argument(
-            '--theory-dl',  action='store_true', required=False,
-            help='load DL theory')
+            "--assume",
+            action="store_true",
+            required=False,
+            help="assume queen(3,1) to be true",
+        )
 
         parser.add_argument(
-            '--theory-con',  action='store_true', required=False,
-            help='load clingcon theory')
+            "--pigeons",
+            action="store_true",
+            required=False,
+            help="add parmeters for the pigeon example holes=3 pigeons=2",
+        )
+        parser.add_argument(
+            "--external",
+            action="store_true",
+            required=False,
+            help="assign external atom `enable` with True in the external example",
+        )
+
+        parser.add_argument(
+            "--theory-dl", action="store_true", required=False, help="load DL theory"
+        )
+
+        parser.add_argument(
+            "--theory-con",
+            action="store_true",
+            required=False,
+            help="load clingcon theory",
+        )
         args = parser.parse_args()
 
         response = requests.get(server)
         print(response.text)
 
         # create solver
-        response = requests.get(server+'create')
+        response = requests.get(server + "create")
         if response.status_code == 500:
             print("Solver already running ...")
             print("Shutting down old solver ...")
-            response = requests.get(server+'close')
-            response = requests.get(server+'create')
+            response = requests.get(server + "close")
+            response = requests.get(server + "create")
             print(response.text)
 
         # register theory
         if args.theory_dl:
-            response = requests.get(server+'register_dl_theory')
+            response = requests.get(server + "register_dl_theory")
             print(response.text)
         if args.theory_con:
-            response = requests.get(server+'register_con_theory')
+            response = requests.get(server + "register_con_theory")
             print(response.text)
 
         # add logic program
-        with open(args.input, 'rb') as f:
-            response = requests.post(server+'add', data=f.read(),
-                                     headers={
-                "Content-Type": "text/plain; charset=utf-8 "}
+        with open(args.input, "rb") as f:
+            response = requests.post(
+                server + "add",
+                data=f.read(),
+                headers={"Content-Type": "text/plain; charset=utf-8 "},
             )
             print(response.text)
 
         # set configuration
         if args.conf:
-            response = requests.post(server+'set_configuration', data=io.StringIO(conf).read(),
-                                     headers={
-                "Content-Type": "application/json; charset=utf-8 "}
+            response = requests.post(
+                server + "set_configuration",
+                data=io.StringIO(conf).read(),
+                headers={"Content-Type": "application/json; charset=utf-8 "},
             )
             print(response.text)
 
             # get configuration
-            response = requests.get(server+'configuration')
+            response = requests.get(server + "configuration")
             dictionary = response.json()
             json_formatted_str = json.dumps(dictionary, indent=2)
             print("Configuration:", json_formatted_str)
@@ -130,9 +149,10 @@ def main():
         if args.pigeons:
             part = '{"pigeon": ["3", "2"]}'
 
-        response = requests.post(server+'ground', data=io.StringIO(part).read(),
-                                 headers={
-            "Content-Type": "application/json; charset=utf-8 "}
+        response = requests.post(
+            server + "ground",
+            data=io.StringIO(part).read(),
+            headers={"Content-Type": "application/json; charset=utf-8 "},
         )
         print(response.text)
 
@@ -140,9 +160,10 @@ def main():
             # set external atom 'enable' to True
             # works with external_test.lp
             assignment = '{"literal": "enable", "truth_value": "True"}'
-            response = requests.post(server+'assign_external', data=io.StringIO(assignment).read(),
-                                     headers={
-                "Content-Type": "application/json; charset=utf-8 "}
+            response = requests.post(
+                server + "assign_external",
+                data=io.StringIO(assignment).read(),
+                headers={"Content-Type": "application/json; charset=utf-8 "},
             )
             print(response.text)
 
@@ -150,10 +171,11 @@ def main():
         if args.assume:
             assumptions = '[["queen(3,1)",true]]'  # works with queens.lp
         else:
-            assumptions = '[]'
-        response = requests.post(server+'solve_with_assumptions', data=io.StringIO(assumptions).read(),
-                                 headers={
-            "Content-Type": "application/json; charset=utf-8 "}
+            assumptions = "[]"
+        response = requests.post(
+            server + "solve_with_assumptions",
+            data=io.StringIO(assumptions).read(),
+            headers={"Content-Type": "application/json; charset=utf-8 "},
         )
         print(response.text)
         poll_models()
@@ -162,15 +184,16 @@ def main():
             # release external atom 'enable'
             # works with external_test.lp
             atom = '"enable"'
-            response = requests.post(server+'release_external', data=io.StringIO(atom).read(),
-                                     headers={
-                "Content-Type": "application/json; charset=utf-8 "}
+            response = requests.post(
+                server + "release_external",
+                data=io.StringIO(atom).read(),
+                headers={"Content-Type": "application/json; charset=utf-8 "},
             )
             print(response.text)
 
         # get statistics
         if args.stats:
-            response = requests.get(server+'statistics')
+            response = requests.get(server + "statistics")
             dictionary = response.json()
             json_formatted_str = json.dumps(dictionary, indent=2)
             print("Statistics:", json_formatted_str)
@@ -182,27 +205,26 @@ def main():
 
 
 def poll_models():
-    ''' poll for models '''
+    """poll for models"""
     count = 0
     while True:
-        response = requests.get(
-            server+'model', timeout=1)
+        response = requests.get(server + "model", timeout=1)
 
         if response.status_code == 200:
             json_response = response.json()
 
-            if json_response == 'Running':
+            if json_response == "Running":
                 print("No model yet ... waiting 10 seconds.")
                 time.sleep(10)
-            elif json_response == 'Done':
+            elif json_response == "Done":
                 print("Search finished, no more models.")
                 break
-            elif 'Model' in json_response:
-                model = json_response['Model']
+            elif "Model" in json_response:
+                model = json_response["Model"]
                 count += 1
-                print("Model", count, ':')
+                print("Model", count, ":")
                 print(bytes(model).decode("utf-8"))
-                response = requests.get(server+'resume')
+                response = requests.get(server + "resume")
                 print(response.text)
             else:
                 print("Error unexpected response to model/ request")
@@ -213,9 +235,9 @@ def poll_models():
             print(response.text)
             break
 
-    response = requests.get(server+'close')
+    response = requests.get(server + "close")
     print(response.text)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
